d626abfac5564db6316d9e15d08dcbda8f8d8678
Arima-cyber <lengocbinhan1235@gmail.com>
2026-01-13 22:52:39 +0700

    Create three.js

---
+++ three.js
@@ -0,0 +1,6 @@
+function showSection(id) {
+  document.querySelectorAll('.content').forEach(sec => {
+    sec.classList.remove('active');
+  });
+  document.getElementById(id).classList.add('active');
+}
